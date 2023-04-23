e325b8b4f6735471a06809dd9a390bb9c2647210
yluiz <luizvictor1231@gmail.com>
2023-04-23 13:24:40 -0400

    Initial development branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { Route, Routes } from 'react-router';
+import Members from './pages/members';
 import './App.css';
+
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <main className="App">
+      <Router>
+        <Routes>
+          <Route path='/' element={ <Members /> }></Route>
+        </Routes>
+      </Router>
+      
+    </main>
   )
 }
 
