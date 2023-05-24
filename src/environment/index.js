fa76a37d5fff7991cc79a608eeb813a71f79c2b4
yluiz <luizvictor1231@gmail.com>
2023-05-24 11:13:10 -0400

    Layout adjust

--- src/environment/index.js
+++ src/environment/index.js
@@ -1,3 +1,3 @@
 export default {
-    API_URL: import.meta.env.VITE_API_URL || 'https://kronosapi-production-46a5.up.railway.app'
+    API_URL: import.meta.env.VITE_API_URL || 'http://localhost:4000'
 }
